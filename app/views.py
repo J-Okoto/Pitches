be2dc95699d199c872fac0e5a4359fa58eb5fe85
J-Okoto <okotojr@gmail.com>
2020-09-20 16:18:01 +0300

    added error function and updated the error html file

--- app/views.py
+++ app/views.py
@@ -1,13 +1,17 @@
-from flask import render_template
+from flask import render_template,request, redirect, url_for, abort
 from app import app
 
 # Views
 @app.route('/')
 def index():
-
     '''
     View root page function that returns the index page and its data
     '''
+    title = 'Home - Welcome to The best Pitching Website Online'
 
-    title = 'Home - Welcome to The best Movie Review Website Online'
+    
+
     return render_template('index.html', title = title)
+
+
+
