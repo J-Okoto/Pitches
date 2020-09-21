59d67a68a716ac280d0b475b79c6c6dee737155b
J-Okoto <okotojr@gmail.com>
2020-09-21 17:18:32 +0300

    added model classes User,roles and comments and created relationships

--- app/models.py
+++ app/models.py
@@ -34,6 +34,7 @@ class Pitch(db.Model):
         category passed to it
         '''
         return Pitch.query.filter_by(category_id= cat_id)
+
 class PitchCategory(db.Model):
     '''
     Function that defines different categories of pitches
@@ -52,3 +53,68 @@ class PitchCategory(db.Model):
         '''
         categories = PitchCategory.query.all()
         return categories
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True) 
+    email = db.Column(db.String(255),unique = True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    
+    pitch = db.relationship('Pitch',backref = 'users',lazy="dynamic")
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comments(cls,id):
+        reviews = Comment.query.filter_by(pitch_id=id).all()
+        return comments
+
+    
+    def __repr__(self):
+        return f'User {self.username}'
+
+
+class Comment(db.Model):
+
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer,primary_key = True)
+    comment= db.Column(db.String)
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitch.id'))
+    username =  db.Column(db.String)
+    votes= db.Column(db.Integer)
+    
+
+    def save_comment(self):
+        '''
+        Function that saves comments
+        '''
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def clear_comments(cls):
+        Comment.all_comments.clear()
+
+    @classmethod
+    def get_comments(cls,id):
+        comments = Comment.query.filter_by(pitch_id=id).all()
+
+        return comments
+        
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic") 
+
+    def __repr__(self):
+        return f'User {self.name}'  
--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app,db
 from flask_script import Manager,Server
-from app.models import Pitch
+from app.models import Pitch,PitchCategory
 
 # Creating app instance
 app = create_app('development')
@@ -11,7 +11,7 @@ manager.add_command('server',Server)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,Pitch= Pitch )
+    return dict(app = app,db = db,Pitch= Pitch,Pitchcategory=PitchCategory )
 
 manager.add_command('server',Server)
 if __name__ == '__main__':
